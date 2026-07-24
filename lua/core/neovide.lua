3059a8deadb7cc135483bf66ccb15a8e3984cb99
bao <80117447+4e-dev@users.noreply.github.com>
2026-07-23 22:18:15 -0400

    Adjust neovide animations

--- lua/core/neovide.lua
+++ lua/core/neovide.lua
@@ -1,6 +1,7 @@
 if vim.g.neovide then
     vim.opt.guioptions = ""
     vim.opt.linespace = 1
-    vim.g.neovide_scroll_animation_length = 0.1
-    vim.g.neovide_cursor_animation_length = 0.05
+    vim.g.neovide_refresh_rate = 165
+    vim.g.neovide_scroll_animation_length = 0.02
+    vim.g.neovide_cursor_animation_length = 0.02
 end
