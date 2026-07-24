14ce9a5e9f1823fa14491abf430c0069b18b3d2f
bao <80117447+4e-dev@users.noreply.github.com>
2026-07-23 22:17:42 -0400

    Remove unnecessary descriptions

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,33 +1,41 @@
-vim.keymap.set("n", "<leader>ef", vim.cmd.Ex, {
-  desc = "Open netrw file explorer",
-})
+-- Open netrw file explorer
+vim.keymap.set("n", "<leader>ef", vim.cmd.Ex)
 
+-- Strip trailing whitespace
 vim.keymap.set("n", "<leader>ws", function()
   vim.cmd([[%s/\s\+$//e]])
   vim.cmd("nohlsearch")
-end, {
-  desc = "Strip trailing whitespace",
-})
+end)
 
---local font_name = "JetBrainsMono Nerd Font Mono"
-local font_name = "FiraMono Nerd Font Mono"
-local font_size = 16
+local font_name = "JetBrainsMono Nerd Font Mono"
+local font_size = 20
 
 local function update_font()
     vim.o.guifont = string.format("%s:h%d", font_name, font_size)
 end
 
+-- Set initial font
+update_font()
+
 -- Increase font size (Ctrl+=)
 vim.keymap.set("n", "<C-=>", function()
-    font_size = font_size + 1
+    font_size = font_size + 2
     update_font()
-end, { desc = "Increase font size" })
+end)
 
 -- Decrease font size (Ctrl+-)
 vim.keymap.set("n", "<C-->", function()
-    font_size = math.max(6, font_size - 1) -- prevent tiny sizes
+    font_size = math.max(12, font_size - 2) -- prevent tiny sizes
     update_font()
-end, { desc = "Decrease font size" })
+end)
 
--- Set initial font
-update_font()
+-- Toggle dark/light mode
+vim.keymap.set('n', '<leader>td', function()
+    vim.o.background = 'dark'
+end)
+vim.keymap.set('n', '<leader>tl', function()
+    vim.o.background = 'light'
+end)
+
+-- Keep yank register after pasting over text
+vim.keymap.set("v", "<leader>p", "\"_dP")
